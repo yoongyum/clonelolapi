c7d4a9fc1bdf9d1c91632ec09a4046420f2342bc
pll0123 <102746073+pll0123@users.noreply.github.com>
2022-07-14 23:01:31 +0900

    Initial commit

---
+++ src/main/java/com/clonelol/ClonelolapiApplication.java
@@ -0,0 +1,13 @@
+package com.clonelol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonelolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonelolapiApplication.class, args);
+	}
+
+}
