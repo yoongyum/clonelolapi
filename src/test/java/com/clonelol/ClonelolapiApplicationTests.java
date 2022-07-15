c748f634fa3a5d5a4c14c9c7aabe37c3b8d4d64d
pll0123 <102746073+pll0123@users.noreply.github.com>
2022-07-15 21:00:25 +0900

    FEAT : 챔피언 클래스 추가

--- src/test/java/com/clonelol/ClonelolapiApplicationTests.java
+++ src/test/java/com/clonelol/ClonelolapiApplicationTests.java
@@ -1,13 +1,52 @@
 package com.clonelol;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClonelolapiApplicationTests {
 
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+
+		Json json = new Json();
+		String s = objectMapper.writeValueAsString(json);
+
+		System.out.println("s = " + s);
 	}
 
+	static class Json{
+		private String id;
+		private String password;
+
+		@Override
+		public String toString() {
+			return "Json{" +
+					"id='" + id + '\'' +
+					", password='" + password + '\'' +
+					'}';
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
 }
