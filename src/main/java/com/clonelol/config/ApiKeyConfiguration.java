f895c961ff029dac425bffc5126ecaf3dd2c9697
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-07-16 20:18:46 +0900

    Delete src/main/java/com/clonelol/config directory

--- src/main/java/com/clonelol/config/ApiKeyConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.clonelol.config;
-
-public class ApiKeyConfiguration {
-
-    //개발자용 API KEY 값 (각자 다름요)
-    public static final String DEV_KEY = "RGAPI-eb7927a8-8ab9-4c30-bc57-93c589cf9dd9";
-}
