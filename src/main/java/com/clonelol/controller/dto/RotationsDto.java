ba071c2ece57cb3be841db8259c74d953eff7f20
yoongyum <imshreg@naver.com>
2022-07-16 23:14:17 +0900

    feat: 로테이션 json -> RotationsDto.java 로 변경

---
+++ src/main/java/com/clonelol/controller/dto/RotationsDto.java
@@ -0,0 +1,18 @@
+package com.clonelol.controller.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class RotationsDto {
+    
+    // 로테이션 챔피언 ID 리스트
+    private ArrayList<Integer> freeChampionIds;
+
+    // 신규 유저를 위한 로테이션 챔피언 ID 리스트
+    private ArrayList<Integer> freeChampionIdsForNewPlayers;
+
+    // 신규 유저 기준 레벨 ( 현재 10 레벨로 되어있음. )
+    private int maxNewPlayerLevel;
+}
