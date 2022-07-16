4572ddf8c2f634a8ebebe1a8b2f8cf9bfbd54003
pll0123 <102746073+pll0123@users.noreply.github.com>
2022-07-16 20:06:56 +0900

    FEAT : json 매핑 class

--- src/main/java/com/clonelol/controller/ChampionsController.java
+++ src/main/java/com/clonelol/controller/ChampionsController.java
@@ -4,6 +4,7 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,6 +16,7 @@ import static com.clonelol.ApiConfiguration.CHAMP_INFO;
 import static com.clonelol.ApiConfiguration.CHAMP_ROTATIONS;
 
 @RestController
+@RequestMapping("/lol/api/champion")
 public class ChampionsController {
 
     //개발자용 API KEY 값
@@ -23,7 +25,7 @@ public class ChampionsController {
     BufferedReader br = null;
 
     //모든 챔피언 정보 불러오기
-    @GetMapping("/lol/api/champion/info")
+    @GetMapping("/info")
     public String getChampionList() {
 
         URI uri = UriComponentsBuilder
@@ -42,17 +44,35 @@ public class ChampionsController {
 
 
     //이번주 로테이션 정보 가져오기
-    @GetMapping("/lol/api/champion/rotations")
+    @GetMapping("/rotations")
     public String getFreeChapList(Model model) {
         URI uri = UriComponentsBuilder
-                .fromUriString(CHAMP_ROTATIONS + developKey)//API URI(String)를 여기다 집어넣는다.
+                .fromUriString(CHAMP_ROTATIONS)//API URI(String)를 여기다 집어넣는다.
+                .queryParam(developKey)
                 .encode()
                 .build().toUri();   //String -> URI type 변경.
 
         RestTemplate restTemplate = new RestTemplate();
 
-        RequestEntity<Void> req = RequestEntity.get(uri).build(); //GET 요청으로 보내고 결과 값을 받아옴.
 
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        return result.getBody();
+    }
+
+    @GetMapping("/list")
+    public String getChampList(){
+        URI uri = UriComponentsBuilder
+                .fromUriString(CHAMP_INFO)
+                .encode()
+                .build().toUri();
+
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .build(); //GET 요청으로 보내고 결과 값을 받아옴.
+
+        RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> result = restTemplate.exchange(req, String.class);
 
         return result.getBody();
