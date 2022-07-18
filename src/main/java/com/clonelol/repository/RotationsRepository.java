f77d29e11ebfaada1d47a4c07f34e9e251b269db
yoongyum <imshreg@naver.com>
2022-07-18 16:47:30 +0900

    feat: ID 조회 메서드 생성

--- src/main/java/com/clonelol/repository/RotationsRepository.java
+++ src/main/java/com/clonelol/repository/RotationsRepository.java
@@ -4,7 +4,9 @@ import com.clonelol.entity.Rotations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RotationsRepository extends JpaRepository<Rotations,String> {
-
+    Optional<Rotations> findById(String RotationID);
 }
