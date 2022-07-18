78b4fd31f55c0fef7aa00cca71b1c3fece8b9040
yoongyum <imshreg@naver.com>
2022-07-18 16:44:49 +0900

    feat: updateMaxLevel 생성

--- src/main/java/com/clonelol/entity/Rotations.java
+++ src/main/java/com/clonelol/entity/Rotations.java
@@ -1,15 +1,26 @@
 package com.clonelol.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Rotations {
     @Id
-    private String id = "rotations";    //로테이션 ID
+    private String id;    //로테이션 ID (PK)
 
-    private int maxNewPlayerLevel;
+    private int maxNewPlayerLevel;  //신규유저 레벨
+
+    // 신규 유저 레벨 변경
+    public void updateMaxLevel(int maxNewPlayerLevel){
+        this.maxNewPlayerLevel = maxNewPlayerLevel;
+    }
 }
