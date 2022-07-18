6ad8f0df30b792b590c9f8cdb0b0bf4988eda83a
yoongyum <imshreg@naver.com>
2022-07-18 20:24:29 +0900

    feat: 로테이션 레포지토리 사용

--- src/main/java/com/clonelol/repository/RotationsRepository.java
+++ src/main/java/com/clonelol/repository/RotationsRepository.java
@@ -1,12 +1,12 @@
-//package com.clonelol.repository;
-//
-//import com.clonelol.entity.Rotations;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface RotationsRepository extends JpaRepository<Rotations,String> {
-//    Optional<Rotations> findById(String RotationID);
-//}
+package com.clonelol.repository;
+
+import com.clonelol.entity.Rotations;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RotationsRepository extends JpaRepository<Rotations,String> {
+    Optional<Rotations> findById(String RotationID);
+}
