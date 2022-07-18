ca3cf36d4585bb2dd6a50af0304163547e58d7f1
yoongyum <imshreg@naver.com>
2022-07-18 16:46:40 +0900

    feat: DTO -> Entity 변환하는 메서드 생성

--- src/main/java/com/clonelol/controller/dto/RotationsDto.java
+++ src/main/java/com/clonelol/controller/dto/RotationsDto.java
@@ -1,5 +1,7 @@
 package com.clonelol.controller.dto;
 
+import com.clonelol.entity.Rotations;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -15,4 +17,12 @@ public class RotationsDto {
 
     // 신규 유저 기준 레벨 ( 현재 10 레벨로 되어있음. )
     private int maxNewPlayerLevel;
+
+    @Builder
+    public Rotations convertToEntity(){
+        return Rotations.builder()
+                .id("rotations")
+                .maxNewPlayerLevel(this.maxNewPlayerLevel)
+                .build();
+    }
 }
