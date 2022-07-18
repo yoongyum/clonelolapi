9eb25a2caf2014021ce07a0cff50975ed5875403
yoongyum <imshreg@naver.com>
2022-07-18 20:23:41 +0900

    feat: 로테이션 리스트에 챔피언 추가 로직 과 챔피언 삭제 로직 추가

--- src/main/java/com/clonelol/entity/Rotations.java
+++ src/main/java/com/clonelol/entity/Rotations.java
@@ -27,4 +27,21 @@ public class Rotations {
     public void updateMaxLevel(int maxNewPlayerLevel){
         this.maxNewPlayerLevel = maxNewPlayerLevel;
     }
+
+
+    //로테이션 리스트 추가
+    public void addFreeChampions(Champion champion){
+        this.freeChampions.add(champion);
+        champion.setRotations(this);
+    }
+
+    public void deleteFreeChampions(){
+        //지난주 로테이션 챔피언들의 FK값을 NULL 처리
+        for (Champion freeChampion : freeChampions) {
+            freeChampion.setRotations(null);
+        }
+        //챔피언 리스트 초기화
+        freeChampions = new ArrayList<>();
+    }
+
 }
