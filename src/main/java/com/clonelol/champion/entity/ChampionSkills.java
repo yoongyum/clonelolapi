3824529dd8c4503aacb69d3671ad7665beaac08f
yoongyum <imshreg@naver.com>
2022-07-27 18:30:44 +0900

    feat: ChampionSkills Entity 생성
    아직 @Entity 어노테이션 안붙임

---
+++ src/main/java/com/clonelol/champion/entity/ChampionSkills.java
@@ -0,0 +1,41 @@
+package com.clonelol.champion.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+//@Entity
+public class ChampionSkills {
+
+    @Id
+    @Column(name = "champion_skills_id")
+    private Long id;    //PK
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "max_rank")
+    private int maxrank;
+
+    @Column(name = "tool_tip")
+    private String tooltip;
+
+    @Column(name = "cool_down_burn")
+    private String cooldownBurn;    //레벨당 쿨타임 변화
+
+    @Column(name = "cost_burns")
+    private String costBurns;   //레벨당 마나 소모량 변화
+
+    @Column(name = "range_burn")
+    private String rangeBurn;   //레벨당 스킬 사거리 변화
+
+}
