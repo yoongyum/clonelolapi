52d020aeeed7cec86758fda53055bc8d5fb988e3
yoongyum <imshreg@naver.com>
2022-07-27 17:32:33 +0900

    feat: ActiveSkill 수정

--- src/main/java/com/clonelol/champion/apidto/property/ActiveSkill.java
+++ src/main/java/com/clonelol/champion/apidto/property/ActiveSkill.java
@@ -9,7 +9,10 @@ public class ActiveSkill {
 
     private String id;
     private String name;
+    private String description;
+    private int maxrank;
     private String tooltip;
-    private String cooldownBurn;
-    private String costBurns;
+    private String cooldownBurn;    //레벨당 쿨타임 변화
+    private String costBurns;   //레벨당 마나 소모량 변화
+    private String rangeBurn;   //레벨당 스킬 사거리 변화
 }
