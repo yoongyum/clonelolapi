8868f5da77d0ccd5b95b12972bd5217761382b5e
yoongyum <imshreg@naver.com>
2022-08-04 16:10:03 +0900

    FEAT: 매치 아이디 저장 Entity 생성

---
+++ src/main/java/com/clonelol/summoner/entity/MatchSimpleInfo.java
@@ -0,0 +1,21 @@
+package com.clonelol.summoner.entity;
+
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Entity
+public class MatchSimpleInfo {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    
+    private String matchId;
+    
+    private Boolean checked = false;    //이미 사용한 매치 데이터인지 확인 - 사용한 뒤에는 true로 바꿔줘야함
+}
