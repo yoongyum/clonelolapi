daceffc4fbd2a8c16416753358e0cc3e3a96bd74
yoongyum <imshreg@naver.com>
2022-08-04 16:30:30 +0900

    FEAT: 매치 ID 데이터수집 API 생성을 위한 Service 로직 구현

--- src/main/java/com/clonelol/summoner/service/MatchService.java
+++ src/main/java/com/clonelol/summoner/service/MatchService.java
@@ -1,8 +1,13 @@
 package com.clonelol.summoner.service;
 
+import com.clonelol.summoner.entity.MatchSimpleInfo;
 import com.clonelol.summoner.repository.MatchSimpleInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -10,5 +15,15 @@ public class MatchService {
 
     private final MatchSimpleInfoRepository matchSimpleInfoRepository;
 
-
+    @Transactional
+    public void initializeAll(List<String> matchInfo){
+        List<MatchSimpleInfo> foundMatchID = new ArrayList<>();
+        matchInfo.stream().forEach(id->{
+            matchSimpleInfoRepository.findByMatchId(id).orElseGet(()->{
+                foundMatchID.add(new MatchSimpleInfo(id));
+                return null;
+            });
+        });
+        matchSimpleInfoRepository.saveAll(foundMatchID);
+    }
 }
