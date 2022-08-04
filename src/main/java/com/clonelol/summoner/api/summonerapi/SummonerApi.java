55f5d21f367902f7c3cf4b79a41bdcc3d3f9fa44
yoongyum <imshreg@naver.com>
2022-08-04 16:30:13 +0900

    FEAT: 매치 ID 데이터수집 API 생성

--- src/main/java/com/clonelol/summoner/api/summonerapi/SummonerApi.java
+++ src/main/java/com/clonelol/summoner/api/summonerapi/SummonerApi.java
@@ -3,6 +3,7 @@ package com.clonelol.summoner.api.summonerapi;
 import com.clonelol.summoner.api.summonerapi.dto.SummonerApiDto;
 import com.clonelol.summoner.entity.SummonerSimpleInfo;
 import com.clonelol.summoner.repository.SummonerSimpleInfoRepository;
+import com.clonelol.summoner.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
@@ -13,10 +14,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.clonelol.config.ApiKeyConfiguration.USER_SOLO_RANK;
+import static com.clonelol.config.ApiKeyConfiguration.*;
 import static java.util.Objects.requireNonNull;
 
 @Slf4j
@@ -26,11 +28,14 @@ public class SummonerApi {
 
     private final RestTemplate restTemplate;
     private final SummonerSimpleInfoRepository summonerSimpleInfoRepository;
+    private final MatchService matchService;
+
+
     @RequestMapping("/summoner")
     public void searchSummonerApi(/*String tier, String division*/){
         URI uri = createUriComponent(USER_SOLO_RANK)
                 .encode()
-                .queryParam("api_key", "RGAPI-718e4463-596c-4509-aac8-5e5851a070cb")
+                .queryParam("api_key", DEV_KEY)
                 .buildAndExpand("DIAMOND", "I")
                 .toUri();
 
@@ -44,6 +49,23 @@ public class SummonerApi {
 
         summonerSimpleInfoRepository.saveAll(simpleInfoList);
     }
+
+    @RequestMapping("/match")
+    public void searchMatchApi(){
+        URI uri = createUriComponent(MATCH_ID)
+                .encode()
+                .queryParam("queue",420)
+                .queryParam("type","ranked")
+                .queryParam("start",0)
+                .queryParam("count",100)
+                .queryParam("api_key", DEV_KEY)
+                .buildAndExpand(/*puuid*/)
+                .toUri();
+
+        List<String> result = restTemplate.getForObject(uri, ArrayList.class);
+        matchService.initializeAll(result);
+    }
+
 
     private UriComponentsBuilder createUriComponent(String uri) {
         return UriComponentsBuilder
