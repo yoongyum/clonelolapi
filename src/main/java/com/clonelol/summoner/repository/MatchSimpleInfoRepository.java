5588e3822296d0365e8125a4ec50837d61c727cf
yoongyum <imshreg@naver.com>
2022-08-04 16:29:49 +0900

    FEAT: 쿼리 메서드 추가 (matchId로 검색)

--- src/main/java/com/clonelol/summoner/repository/MatchSimpleInfoRepository.java
+++ src/main/java/com/clonelol/summoner/repository/MatchSimpleInfoRepository.java
@@ -3,5 +3,8 @@ package com.clonelol.summoner.repository;
 import com.clonelol.summoner.entity.MatchSimpleInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MatchSimpleInfoRepository extends JpaRepository<MatchSimpleInfo,Long> {
+    Optional<MatchSimpleInfo> findByMatchId(String matchId);
 }
