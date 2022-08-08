1a4cbb71c8b0b4cc4435e6523919ed56dcb0bfe7
yoongyum <imshreg@naver.com>
2022-08-08 21:09:00 +0900

    FEAT: public -> private으로 변경/ Entity변경 함수 생성

--- src/main/java/com/clonelol/summoner/api/summonerapi/dto/SummonerIdInfoDto.java
+++ src/main/java/com/clonelol/summoner/api/summonerapi/dto/SummonerIdInfoDto.java
@@ -1,5 +1,6 @@
 package com.clonelol.summoner.api.summonerapi.dto;
 
+import com.clonelol.summoner.entity.SummonerIdInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
@@ -8,10 +9,19 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SummonerIdInfoDto {
     @JsonProperty("id")
-    public String summonerId;
+    private String summonerId;
 
-    public String accountId;
+    private String accountId;
 
     @JsonProperty("puuid")
-    public String puuId;
+    private String puuId;
+
+    // DTO -> Entity
+    public SummonerIdInfo convertToEntity(){
+        return SummonerIdInfo.builder()
+                .summonerId(this.summonerId)
+                .accountId(this.accountId)
+                .puuId(this.puuId)
+                .build();
+    }
 }
