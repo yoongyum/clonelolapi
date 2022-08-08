596dcd3826e169d7c6113058933626d442789d8a
yoongyum <imshreg@naver.com>
2022-08-08 21:10:04 +0900

    FEAT: SummonerIdInfo Entity 와 Repository 생성

---
+++ src/main/java/com/clonelol/summoner/repository/SummonerIdInfoRepository.java
@@ -0,0 +1,6 @@
+package com.clonelol.summoner.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SummonerIdInfoRepository extends JpaRepository<SummonerIdInfoRepository, String> {
+}
