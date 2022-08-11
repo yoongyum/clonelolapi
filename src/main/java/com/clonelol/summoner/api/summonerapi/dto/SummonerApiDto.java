88831812cbb5e361581304115c7ff2ed96041a71
yoongyum <imshreg@naver.com>
2022-08-11 14:29:07 +0900

    FIX: SummonerApi Dto 수정

--- src/main/java/com/clonelol/summoner/api/summonerapi/dto/SummonerApiDto.java
+++ src/main/java/com/clonelol/summoner/api/summonerapi/dto/SummonerApiDto.java
@@ -1,21 +1,12 @@
 package com.clonelol.summoner.api.summonerapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SummonerApiDto {
 
-    private String leagueId;
-    private String queueType;
-    private String tier;
-    private String rank;
     private String summonerId;
-    private String summonerName;
-    private int leaguePoints;
-    private int wins;
-    private int losses;
-    private boolean veteran;
-    private boolean inactive;
-    private boolean freshBlood;
-    private boolean hotStreak;
+    @JsonProperty("puuid")
+    private String pUuid;
 }
