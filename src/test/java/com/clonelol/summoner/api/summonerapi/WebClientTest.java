1d8ff9161069753e9c0b537a757821e41dd1eb63
yoongyum <imshreg@naver.com>
2022-08-14 21:12:37 +0900

    CONFIG: exchangeTo 테스트 완료

--- src/test/java/com/clonelol/summoner/api/summonerapi/WebClientTest.java
+++ src/test/java/com/clonelol/summoner/api/summonerapi/WebClientTest.java
@@ -4,27 +4,51 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import static com.clonelol.config.ApiKeyConfiguration.GAME_VERSION;
 
 @Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
 public class WebClientTest {
 
     @Autowired
-    private WebClient webClient;
+    private WebClient.Builder webClient;
 
     @Test
     @DisplayName("webclient 도입 테스트")
     public void test2() {
-        webClient = WebClient.create();
-        String[] list = webClient.get()
-                .uri(GAME_VERSION)
+        log.info("retrieve 사용");
+        String[] list = webClient.baseUrl(GAME_VERSION).build().get()
+                .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(String[].class)
                 .block();
-        for (var s : list){
-            System.out.println(s);
-        }
+
+        assert list != null;
+        log.info("최신버전: {}", list[0]);
+
+        log.info("exchangeTo 사용");
+        String[] list2 = webClient.build().get()
+                .uri(GAME_VERSION)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchangeToMono(res -> {
+                    if (res.statusCode().equals(HttpStatus.OK)) {
+                        return res.bodyToMono(String[].class);
+                    } else {
+                        return res.createException().flatMap(Mono::error);
+                    }
+                }).block();
+
+        assert list2 != null;
+        log.info("최신버전: {}", list2[0]);
     }
+
+
 }
