70674fcb07e962ad6a199a078a201f7749ac5539
jungsunghun <jsh951227@gmail.com>
2022-08-15 22:06:06 +0900

    feat : restTemplate -> webClient로 변경

--- src/main/java/com/clonelol/champion/api/ChampionsController.java
+++ src/main/java/com/clonelol/champion/api/ChampionsController.java
@@ -1,8 +1,5 @@
 package com.clonelol.champion.api;
 
-import com.clonelol.champion.api.ChampInformationDto;
-import com.clonelol.champion.api.DetailInfoDto;
-import com.clonelol.champion.api.SimpleInfoDto;
 import com.clonelol.champion.entity.Champion;
 import com.clonelol.champion.entity.Version;
 import com.clonelol.champion.repository.VersionRepository;
@@ -13,7 +10,9 @@ import org.springframework.context.event.EventListener;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -22,14 +21,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.clonelol.config.ApiKeyConfiguration.*;
+import static java.util.Objects.requireNonNull;
 
 @Component
+@Controller
 @RequiredArgsConstructor
 public class ChampionsController {
 
     private final RestTemplate restTemplate;
     private final ChampionsService championsService;
     private final VersionRepository versionRepository;
+    private final WebClient.Builder webBuilder;
     private final static List<Champion> freeChampion = new ArrayList<>();
 
     //서버 켜지면 버전이 최신인지 확인 후
@@ -42,11 +44,11 @@ public class ChampionsController {
         //저장된 데이터가
         //있으면 그대로 가져오고, 없으면 최신 버전을 생성하면서 챔프 정보를 불러온다.
         Version version = versionRepository.findById("Version")
-                .orElseGet(()-> create(newVersion));
+                .orElseGet(() -> create(newVersion));
 
         //저장된 데이터가 있을 때,
         //최신 버전인지 확인하고 아니라면 업데이트 한다.
-        if(!version.isLatestVersion(newVersion)){
+        if (!version.isLatestVersion(newVersion)) {
             updateChamp4newVersion(newVersion, version);
         }
 
@@ -89,7 +91,8 @@ public class ChampionsController {
                 .build();
 
         ChampInformationDto<SimpleInfoDto> champList = restTemplate
-                .exchange(build, new ParameterizedTypeReference<ChampInformationDto<SimpleInfoDto>>() {})
+                .exchange(build, new ParameterizedTypeReference<ChampInformationDto<SimpleInfoDto>>() {
+                })
                 .getBody();
 
         List<Champion> entityList = champList.getNameSet()
@@ -103,21 +106,19 @@ public class ChampionsController {
 
 
     // 각 챔피언의 세부정보 받아오는 API 호출 메서드
-    private DetailInfoDto searchChampDetail(String version, String champName) {
-        //String -> URI type 변경.
-        URI url = createUriComponent(CHAMP_DETAILS)
-                .encode()
-                .buildAndExpand(version, champName)
-                .toUri();
+    public DetailInfoDto searchChampDetail(String version, String champName) {
 
-        RequestEntity<Void> request = RequestEntity.get(url)
-                .build();
-
-        ChampInformationDto<DetailInfoDto> infoList = restTemplate.exchange(request, new ParameterizedTypeReference<ChampInformationDto<DetailInfoDto>>() {
+        return requireNonNull(
+                webBuilder
+                        .baseUrl(GAME_DATA)
+                        .build()
+                        .get()
+                        .uri(builder -> builder.path(CHAMP_DETAILS)
+                                .build(version, champName))
+                        .retrieve())
+                .bodyToMono(new ParameterizedTypeReference<ChampInformationDto<DetailInfoDto>>() {
                 })
-                .getBody();
-
-        return infoList.getValue(champName);
+                .block().getValue(champName);
     }
 
     private UriComponentsBuilder createUriComponent(String uri) {
