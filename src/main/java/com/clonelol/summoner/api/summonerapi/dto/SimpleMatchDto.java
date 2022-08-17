bf8926427be23130e6c8b2cb3004fe1b1280bd92
yoongyum <imshreg@naver.com>
2022-08-17 15:20:44 +0900

    FEAT: SimpleMatchDto 생성

---
+++ src/main/java/com/clonelol/summoner/api/summonerapi/dto/SimpleMatchDto.java
@@ -0,0 +1,12 @@
+package com.clonelol.summoner.api.summonerapi.dto;
+
+import com.clonelol.summoner.api.summonerapi.dto.property.Info;
+import com.clonelol.summoner.api.summonerapi.dto.property.Metadata;
+
+public class SimpleMatchDto {
+
+    private Metadata metadata;
+
+    private Info info;
+
+}
