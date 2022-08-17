a29e1e6b94e7cfd9001f33cc43da5cfacd1a4150
yoongyum <imshreg@naver.com>
2022-08-17 16:12:41 +0900

    REFACTOR: @JsonIgnoreProperties 추가

--- src/main/java/com/clonelol/summoner/api/summonerapi/dto/SimpleMatchDto.java
+++ src/main/java/com/clonelol/summoner/api/summonerapi/dto/SimpleMatchDto.java
@@ -2,7 +2,9 @@ package com.clonelol.summoner.api.summonerapi.dto;
 
 import com.clonelol.summoner.api.summonerapi.dto.property.Info;
 import com.clonelol.summoner.api.summonerapi.dto.property.Metadata;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SimpleMatchDto {
 
     private Metadata metadata;
