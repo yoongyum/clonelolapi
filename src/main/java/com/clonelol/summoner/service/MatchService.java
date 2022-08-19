cd45feac4221687feea4d8415968230fde15047c
jungsunghun <jsh951227@gmail.com>
2022-08-19 16:17:14 +0900

    -

--- src/main/java/com/clonelol/summoner/service/MatchService.java
+++ src/main/java/com/clonelol/summoner/service/MatchService.java
@@ -5,7 +5,6 @@ import com.clonelol.summoner.api.summonerapi.dto.MatchSummaryDto;
 import com.clonelol.summoner.entity.MatchSimpleInfo;
 import com.clonelol.summoner.entity.MatchSummary;
 import com.clonelol.summoner.repository.MatchSimpleInfoRepository;
-import com.clonelol.summoner.repository.MatchSummaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,14 +17,13 @@ import java.util.List;
 public class MatchService {
 
     private final MatchSimpleInfoRepository matchSimpleInfoRepository;
-    private final MatchSummaryRepository matchSummaryRepository;
     private final ChampionRepository championRepository;
 
     @Transactional
-    public void initializeAll(List<String> matchInfo){
+    public void initializeAll(List<String> matchInfo) {
         List<MatchSimpleInfo> foundMatchID = new ArrayList<>();
-        matchInfo.stream().forEach(id->{
-            matchSimpleInfoRepository.findByMatchId(id).orElseGet(()->{
+        matchInfo.forEach(id -> {
+            matchSimpleInfoRepository.findByMatchId(id).orElseGet(() -> {
                 foundMatchID.add(new MatchSimpleInfo(id));
                 return null;
             });
@@ -37,9 +35,10 @@ public class MatchService {
     public void initializeAllForSummary(List<MatchSummaryDto> matchSummaryDtos) {
         matchSummaryDtos.forEach(matchSummaryDto -> {
             MatchSummary matchSummary = matchSummaryDto.convertToEntity();
-            championRepository.findById(matchSummaryDto.getChampId()).ifPresent(champion -> {
-                champion.addMatchSummary(matchSummary);
-            });
+            championRepository.findById(matchSummaryDto.getChampId())
+                    .ifPresent(champion -> champion.addMatchSummary(matchSummary));
         });
     }
+
+
 }
